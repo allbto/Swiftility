0aca6389208fb5952724c54320ed1c5a6cfd34c0
Allan Barbato <allan.barbato@gmail.com>
2015-12-15 18:13:04 +0100

    Add: Handling dispatch_after in Closure

--- Swiftility/Swiftility/Classes/Closure.swift
+++ Swiftility/Swiftility/Classes/Closure.swift
@@ -24,6 +24,12 @@ public func async_main(block: BasicClosure)
     dispatch_async(dispatch_get_main_queue(), block)
 }
 
+/// Convenience call to dispatch_after (time is in seconds)
+public func after(time: Double, queue: dispatch_queue_t = dispatch_get_main_queue(), closure: BasicClosure)
+{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(UInt64(time) * NSEC_PER_SEC)), queue, closure)
+}
+
 // MARK: - Debounce
 
 /// Convenience call to dispatch_debounce_block
