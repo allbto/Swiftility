a8f3e4dde77e75d094e0bc2182236392146d0d19
Allan Barbato <allan.barbato@gmail.com>
2015-12-16 19:56:31 +0100

    Add: ViewController subscribing for keyboard notifications

--- Swiftility/Swiftility/Classes/Extensions/UIKit/UIViewControllerExtensions.swift
+++ Swiftility/Swiftility/Classes/Extensions/UIKit/UIViewControllerExtensions.swift
@@ -29,6 +29,51 @@ extension UIViewController
         self.presentViewController(alertController, animated: true, completion: nil)
     }
     
+    // MARK: - Keyboard
+    
+    /**
+    Subscribe to keyboard updates notifications.
+    Update constraint if != nil.
+    Call updateClosure if != nil.
+    If both == nil does nothing.
+    
+    - parameter constraint:    the contraint to update when keyboard change frame or hide
+    - parameter updateClosure: called when keyboard change frame or hide
+    */
+    public func subscribeToKeyboardUpdates(constraint: NSLayoutConstraint?, updateClosure: ((NSNotification) -> Void)? = nil)
+    {
+        guard constraint != nil || updateClosure != nil else { return }
+        
+        NSNotificationCenter.defaultCenter().addObserverWithNames([UIKeyboardWillChangeFrameNotification, UIKeyboardWillHideNotification]) { [weak self] n in
+            if let constraint = constraint {
+                self?.adjustKeyboardContraint(constraint, notification: n)
+            }
+            
+            updateClosure?(n)
+        }
+    }
+    
+    private func adjustKeyboardContraint(constraint: NSLayoutConstraint, notification n: NSNotification)
+    {
+        guard let userInfo = n.userInfo,
+            keyboardSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue().size,
+            duration = userInfo[UIKeyboardAnimationDurationUserInfoKey]?.doubleValue,
+            curve = userInfo[UIKeyboardAnimationCurveUserInfoKey]?.unsignedIntValue
+            else { return }
+        
+        if n.name == UIKeyboardWillHideNotification {
+            constraint.constant = 0
+        } else {
+            constraint.constant = keyboardSize.height
+        }
+        
+        let options = UIViewAnimationOptions(rawValue: UInt(curve) << 16)
+        
+        UIView.animateWithDuration(duration, delay: 0, options: options, animations: {
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+    }
+    
     // MARK: - HUD
     
 //    var defaultHUD: JGProgressHUD? {
