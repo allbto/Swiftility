2311d7cad1069fa4b7e5c61f6d3ff4982ceff88c
Allan Barbato <allan.barbato@gmail.com>
2016-01-28 11:03:20 +0100

    Refacto: WeakTimer

--- Swiftility/Swiftility/Classes/Types/WeakTimer.swift
+++ Swiftility/Swiftility/Classes/Types/WeakTimer.swift
@@ -9,24 +9,45 @@
 import Foundation
 
 /// A factory for NSTimer instances that invoke closures, thereby allowing a weak reference to its context.
-public struct WeakTimerFactory {
-    public class WeakTimer: NSObject {
-        private var timer: NSTimer!
-        private let callback: () -> Void
-        
-        private init(timeInterval: NSTimeInterval, userInfo: AnyObject?, repeats: Bool, callback: () -> Void) {
-            self.callback = callback
-            super.init()
-            self.timer = NSTimer.scheduledTimerWithTimeInterval(timeInterval, target: self, selector: "invokeCallback", userInfo: userInfo, repeats: repeats)
-        }
-        
-        func invokeCallback() {
-            callback()
-        }
-    }
+public class WeakTimer: NSObject
+{
+    // MARK: - Properties
     
-    /// Returns a new timer that has not yet executed, and is not scheduled for execution.
-    public static func timerWithTimeInterval(timeInterval: NSTimeInterval, userInfo: AnyObject?, repeats: Bool, callback: () -> Void) -> NSTimer {
+    private var timer: NSTimer!
+    private let callback: () -> Void
+
+    // MARK: - Life cycle
+    
+    private init(timeInterval: NSTimeInterval, userInfo: AnyObject?, repeats: Bool, callback: () -> Void)
+    {
+        self.callback = callback
+        super.init()
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(timeInterval, target: self, selector: "invokeCallback", userInfo: userInfo, repeats: repeats)
+    }
+
+    // MARK: - Actions
+    
+    func invokeCallback()
+    {
+        callback()
+    }
+
+    // MARK: - Public
+    
+    /**
+    Returns a new timer that has not yet executed, and is not scheduled for execution
+    More info, see: NSTimer.scheduledTimerWithTimeInterval
+    
+    - parameter timeInterval: Interval
+    - parameter userInfo:     =nil; userInfo object
+    - parameter repeats:      =false; Should repeat or called only once
+    - parameter callback:     callback called instead of NSTimer target's selector
+    
+    - returns: new scheduled timer
+    */
+    public static func scheduledTimerWithTimeInterval(timeInterval: NSTimeInterval, userInfo: AnyObject? = nil, repeats: Bool = false, callback: () -> Void) -> NSTimer
+    {
         return WeakTimer(timeInterval: timeInterval, userInfo: userInfo, repeats: repeats, callback: callback).timer
     }
 }
+
