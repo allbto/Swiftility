bfba253a50c162ae28f5371791fd2d7e683a4aa5
allbto <allan.barbato@gmail.com>
2016-09-20 14:16:42 +0200

    Add: Package.swift for SPM

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  Swiftility
+//
+//  Created by Allan Barbato on 9/20/16.
+//  Copyright © 2016 Allan Barbato. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Swiftility"
+)
