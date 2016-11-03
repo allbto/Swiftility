5f325746261432657609d9f81c1dcc8ff9ddffd3
allbto <allan.barbato@gmail.com>
2016-11-03 11:31:03 +0100

    Update: swizzle -> Objc.swizzle

---
+++ Swiftility/Sources/Core/ObjcExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  swizzle.swift
+//  Swiftility
+//
+//  Created by Allan Barbato on 9/26/16.
+//  Copyright © 2016 Allan Barbato. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Swizzle
+extension ObjC
+{
+    public func swizzle(_ originalSelector: Selector, with swizzleSelector: Selector, on object: AnyClass)
+    {
+        let originalMethod = class_getInstanceMethod(object, originalSelector)
+        let swizzleMethod = class_getInstanceMethod(object, swizzleSelector)
+        
+        if class_addMethod(object, originalSelector, method_getImplementation(swizzleMethod), method_getTypeEncoding(swizzleMethod)) {
+            class_replaceMethod(object, swizzleSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+        } else {
+            method_exchangeImplementations(originalMethod, swizzleMethod)
+        }
+    }
+}
