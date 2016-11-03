1c5b97958e68d991df4a5fd637044341b57a4146
allbto <allan.barbato@gmail.com>
2016-11-03 14:11:56 +0100

    Fix: Header issue

--- Swiftility/Sources/Core/ObjcExtensions.swift
+++ Swiftility/Sources/Core/ObjcExtensions.swift
@@ -9,17 +9,14 @@
 import Foundation
 
 // MARK: Swizzle
-extension ObjC
+public func swizzle(_ originalSelector: Selector, with swizzleSelector: Selector, on object: AnyClass)
 {
-    public static func swizzle(_ originalSelector: Selector, with swizzleSelector: Selector, on object: AnyClass)
-    {
-        let originalMethod = class_getInstanceMethod(object, originalSelector)
-        let swizzleMethod = class_getInstanceMethod(object, swizzleSelector)
-        
-        if class_addMethod(object, originalSelector, method_getImplementation(swizzleMethod), method_getTypeEncoding(swizzleMethod)) {
-            class_replaceMethod(object, swizzleSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
-        } else {
-            method_exchangeImplementations(originalMethod, swizzleMethod)
-        }
+    let originalMethod = class_getInstanceMethod(object, originalSelector)
+    let swizzleMethod = class_getInstanceMethod(object, swizzleSelector)
+    
+    if class_addMethod(object, originalSelector, method_getImplementation(swizzleMethod), method_getTypeEncoding(swizzleMethod)) {
+        class_replaceMethod(object, swizzleSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+    } else {
+        method_exchangeImplementations(originalMethod, swizzleMethod)
     }
 }
