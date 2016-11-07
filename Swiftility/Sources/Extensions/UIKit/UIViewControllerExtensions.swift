9b1d6ee0ab559cac980618c13716656a0f00011a
allbto <allan.barbato@gmail.com>
2016-11-07 12:59:15 +0100

    Add: Convenience setup UIViewController methods

--- Swiftility/Sources/Extensions/UIKit/UIViewControllerExtensions.swift
+++ Swiftility/Sources/Extensions/UIKit/UIViewControllerExtensions.swift
@@ -112,3 +112,129 @@ extension UIViewController
      
      */
 }
+
+// MARK: - Convenience setup
+private var NotificationObserversKey: StaticString = "SW_NotificationObserversKey"
+private var PermanentNotificationObserversKey: StaticString = "SW_PermanentNotificationObserversKey"
+private var ViewHasLoadedObserversKey: StaticString = "SW_ViewHasLoadedObserversKey"
+private var ViewHasAppearedObserversKey: StaticString = "SW_ViewHasAppearedObserversKey"
+
+extension UIViewController
+{
+    // MARK: - Private Properties
+    
+    fileprivate var _notificationObservers: [NSObjectProtocol]? {
+        get { return objc_getAssociatedObject(self, &NotificationObserversKey) as? [NSObjectProtocol] }
+        set { objc_setAssociatedObject(self, &NotificationObserversKey, newValue as [NSObjectProtocol]?, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+    
+    fileprivate var _permanentNotificationObservers: [NSObjectProtocol]? {
+        get { return objc_getAssociatedObject(self, &PermanentNotificationObserversKey) as? [NSObjectProtocol] }
+        set { objc_setAssociatedObject(self, &PermanentNotificationObserversKey, newValue as [NSObjectProtocol]?, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+    
+    // MARK: - Public Properties
+    
+    public fileprivate(set) var viewHasLoaded: Bool {
+        get { return (objc_getAssociatedObject(self, &ViewHasLoadedObserversKey) as? Bool) ?? false }
+        set { objc_setAssociatedObject(self, &ViewHasLoadedObserversKey, newValue as Bool?, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+    
+    public fileprivate(set) var viewHasAppeared: Bool {
+        get { return (objc_getAssociatedObject(self, &ViewHasAppearedObserversKey) as? Bool) ?? false }
+        set { objc_setAssociatedObject(self, &ViewHasAppearedObserversKey, newValue as Bool?, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+    
+    // MARK: - Life cycle
+    
+    override open class func initialize()
+    {
+        guard self === UIViewController.self else { return }
+        
+        once(token: "com.swiftility.UIViewControllerInitialize") {
+            swizzle(#selector(UIViewController.viewDidLoad), with: #selector(UIViewController.sw_viewDidLoad), on: self)
+            
+            swizzle(#selector(UIViewController.viewWillAppear(_:)), with: #selector(UIViewController.sw_viewWillAppear(_:)), on: self)
+            swizzle(#selector(UIViewController.viewWillDisappear(_:)), with: #selector(UIViewController.sw_viewWillDisappear(_:)), on: self)
+            
+            swizzle(#selector(UIViewController.viewDidAppear(_:)), with: #selector(UIViewController.sw_viewDidAppear(_:)), on: self)
+            swizzle(#selector(UIViewController.viewDidDisappear(_:)), with: #selector(UIViewController.sw_viewDidDisappear(_:)), on: self)
+        }
+    }
+    
+    func sw_viewDidLoad()
+    {
+        viewHasLoaded = true
+        
+        self.sw_viewDidLoad()
+        
+        self.sw_setupViews()
+        
+        let observers = self.sw_setupPermanentNotificationObservers()
+        if !observers.isEmpty {
+            _permanentNotificationObservers = observers
+        }
+    }
+    
+    func sw_viewWillAppear(_ animated: Bool)
+    {
+        self.sw_viewWillAppear(animated)
+        
+        let observers = self.sw_setupActiveNotificationObservers()
+        if !observers.isEmpty {
+            _notificationObservers = observers
+        }
+    }
+    
+    func sw_viewWillDisappear(_ animated: Bool)
+    {
+        self.sw_viewWillDisappear(animated)
+        
+        if let observers = _notificationObservers, !observers.isEmpty {
+            NotificationCenter.default.removeObservers(observers)
+        }
+    }
+    
+    func sw_viewDidAppear(_ animated: Bool)
+    {
+        viewHasAppeared = true
+        
+        self.sw_viewDidAppear(animated)
+    }
+    
+    func sw_viewDidDisappear(_ animated: Bool)
+    {
+        viewHasAppeared = false
+        
+        self.sw_viewDidDisappear(animated)
+    }
+    
+    // MARK: - Open methods
+    
+    /// Notification observers setup when view appears and removed when view disappears
+    open func sw_setupActiveNotificationObservers() -> [NSObjectProtocol]
+    {
+        return [] // To be overriden
+    }
+    
+    /// Notification observers setup on view load and removed on deinit
+    /// Don't forget to call self.sw_removePermanentObservers() on class deinit
+    open func sw_setupPermanentNotificationObservers() -> [NSObjectProtocol]
+    {
+        return [] // To be overriden
+    }
+    
+    open func sw_setupViews()
+    {
+        // To be overriden
+    }
+    
+    // MARK: - Utils
+    
+    public func sw_removePermanentObservers()
+    {
+        if let observers = _permanentNotificationObservers, !observers.isEmpty {
+            NotificationCenter.default.removeObservers(observers)
+        }
+    }
+}
