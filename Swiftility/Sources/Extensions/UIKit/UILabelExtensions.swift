9ccc9bfd4903bc6cd2633752114527c5ccad1071
allbto <allan.barbato@gmail.com>
2016-12-22 17:04:41 +0100

    Add: UILabel convenience init

---
+++ Swiftility/Sources/Extensions/UIKit/UILabelExtensions.swift
@@ -0,0 +1,27 @@
+//
+//  UILabelExtensions.swift
+//  Swiftility
+//
+//  Created by Allan Barbato on 12/22/16.
+//  Copyright © 2016 Allan Barbato. All rights reserved.
+//
+
+import Foundation
+
+extension UILabel
+{
+    /// Init label with font textColor and textAlignement
+    ///
+    /// - Parameters:
+    ///   - font: Label font
+    ///   - textColor: default UILabel appearence textColor
+    ///   - textAlignment: default .left
+    public convenience init(font: UIFont, textColor: UIColor = UILabel.appearance().textColor, textAlignment: NSTextAlignment = .left)
+    {
+        self.init()
+        
+        self.font = font
+        self.textColor = textColor
+        self.textAlignment = textAlignment
+    }
+}
