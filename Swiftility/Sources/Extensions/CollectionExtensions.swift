7a746b96e1e3734461130508093befad36a477a9
allbto <allan.barbato@gmail.com>
2016-12-22 18:07:35 +0100

    Rename mapOptional -> optionalMap

--- Swiftility/Sources/Extensions/CollectionExtensions.swift
+++ Swiftility/Sources/Extensions/CollectionExtensions.swift
@@ -10,11 +10,9 @@ import Foundation
 
 extension Collection
 {
-    /// Returns an `Array` containing the results of mapping `transform` over `self`.
-    /// + Filters out the nil values.
-    ///
+    /// Returns an `Array` containing the results of mapping `transform` over `self` + Filters out the nil values.
     /// - Complexity: O(N).
-    public func mapOptional<T>(_ transform: (Self.Iterator.Element) throws -> T?) rethrows -> [T]
+    public func optionalMap<T>(_ transform: (Self.Iterator.Element) throws -> T?) rethrows -> [T]
     {
         let count: Int = numericCast(self.count)
         guard count > 0 else { return [] }
